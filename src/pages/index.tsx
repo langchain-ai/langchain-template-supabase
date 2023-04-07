64cd9e763dc285f6fa4f5752b93c46fb6d355616
Nuno Campos <nuno@boringbits.io>
2023-04-07 23:43:09 +0100

    Add ui and streaming support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,61 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import {
+  EventStreamContentType,
+  fetchEventSource,
+} from "@microsoft/fetch-event-source";
+import styles from "@/styles/Home.module.css";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { FormEvent, useCallback, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const supabase = useSupabaseClient();
+  const [stream, setStream] = useState(true);
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [inflight, setInflight] = useState(false);
+
+  const onSubmit = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+
+      if (inflight) return;
+
+      setInflight(true);
+      setOutput("");
+
+      try {
+        if (stream) {
+          await fetchEventSource(
+            `${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/chat`,
+            {
+              method: "POST",
+              body: JSON.stringify({ input }),
+              headers: { "Content-Type": "application/json" },
+              onmessage(ev) {
+                setOutput((o) => o + ev.data);
+              },
+            }
+          );
+          setInput("");
+        } else {
+          const { data } = await supabase.functions.invoke("chat", {
+            body: { input },
+          });
+          setOutput(data);
+          setInput("");
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setInflight(false);
+      }
+    },
+    [input, stream, inflight, supabase]
+  );
+
   return (
     <>
       <Head>
@@ -14,110 +64,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+      <main className={`${styles.main} ${inter.className}`}>
+        <form
+          onSubmit={onSubmit}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <input
+            type="text"
+            placeholder="Ask..."
+            style={{ padding: 5, width: 200, marginBottom: 10 }}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <input
+              type="checkbox"
+              id="stream"
+              style={{ marginRight: 5 }}
+              checked={stream}
+              onChange={() => setStream((s) => !s)}
             />
+            <label htmlFor="stream">Stream</label>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </form>
+        <div style={{ width: 200 }}>Response: {output}</div>
       </main>
     </>
-  )
+  );
 }
--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -1,23 +1,69 @@
 import { serve } from "http/server.ts";
 import { ChatOpenAI } from "langchain/chat_models";
 import { LLMChain } from "langchain/chains";
+import { CallbackManager } from "langchain/callbacks";
 import {
   ChatPromptTemplate,
   HumanMessagePromptTemplate,
 } from "langchain/prompts";
+
+import { corsHeaders } from "../_shared/cors.ts";
 
 const prompt = ChatPromptTemplate.fromPromptMessages([
   HumanMessagePromptTemplate.fromTemplate("{input}"),
 ]);
 
 serve(async (req) => {
-  const { input } = await req.json();
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
 
-  const llm = new ChatOpenAI();
-  const chain = new LLMChain({ prompt, llm });
-  const response = await chain.run(input);
+  try {
+    const { input } = await req.json();
+    const streaming = req.headers.get("accept") === "text/event-stream";
 
-  return new Response(JSON.stringify(response), {
-    headers: { "Content-Type": "application/json" },
-  });
+    if (streaming) {
+      const encoder = new TextEncoder();
+      const stream = new TransformStream();
+      const writer = stream.writable.getWriter();
+
+      const llm = new ChatOpenAI({
+        streaming,
+        callbackManager: CallbackManager.fromHandlers({
+          handleLLMNewToken: async (token) => {
+            await writer.ready;
+            await writer.write(encoder.encode(`data: ${token}\n\n`));
+          },
+          handleLLMEnd: async () => {
+            await writer.ready;
+            await writer.close();
+          },
+          handleLLMError: async (e) => {
+            await writer.ready;
+            await writer.abort(e);
+          },
+        }),
+      });
+      const chain = new LLMChain({ prompt, llm });
+      chain.run(input).catch((e) => console.error(e));
+
+      return new Response(stream.readable, {
+        headers: { ...corsHeaders, "Content-Type": "text/event-stream" },
+      });
+    } else {
+      const llm = new ChatOpenAI();
+      const chain = new LLMChain({ prompt, llm });
+      const response = await chain.run(input);
+
+      return new Response(JSON.stringify(response), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+      });
+    }
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), {
+      status: 500,
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    });
+  }
 });
